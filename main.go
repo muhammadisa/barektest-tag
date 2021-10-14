e7dbe3f5cc935a1ac3dabf9bc0a204146df179fc
Muhammad Isa WK <msawjk@gmail.com>
2021-10-14 22:01:53 +0700

    fixing

--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"fmt"
+	pb "gitlab.com/muhammadisa/barektest-tag/protoc/api/v1"
+	"github.com/soheilhy/cmux"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net"
+	"google.golang.org/grpc"
+)
+
+// ServeGRPC serving GRPC server and will be merged using MergeServer function
+func ServeGRPC(listener net.Listener, service pb.TopicServiceServer, serverOptions []grpc.ServerOption) error {
+	level.Info(gvars.Log).Log(lgr.LogInfo, "initialize grpc server")
+
+	var grpcServer *grpc.Server
+	if len(serverOptions) > 0 {
+		grpcServer = grpc.NewServer(serverOptions...)
+	} else {
+		grpcServer = grpc.NewServer()
+	}
+	pb.RegisterTopicServiceServer(grpcServer, service)
+	return grpcServer.Serve(listener)
+}
+
+// ServeHTTP serving HTTP server and will be merged using MergeServer function
+func ServeHTTP(listener net.Listener, service pb.TopicServiceServer) error {
+	level.Info(gvars.Log).Log(lgr.LogInfo, "initialize rest server")
+
+	mux := runtime.NewServeMux()
+	err := pb.RegisterTopicServiceHandlerServer(context.Background(), mux, service)
+	if err != nil {
+		return err
+	}
+	srv := &http.Server{Handler: hdr.CORS(mux)}
+	return srv.Serve(listener)
+}
+
+// MergeServer start ServeGRPC and ServeHTTP concurrently
+func MergeServer(service pb.TopicServiceServer, serverOptions []grpc.ServerOption) {
+	port := fmt.Sprintf(":%s", "50013")
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := cmux.New(listener)
+	grpcListener := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings(
+		"content-type", "application/grpc",
+	))
+	httpListener := m.Match(cmux.HTTP1Fast())
+
+	g := new(errgroup.Group)
+	g.Go(func() error { return ServeGRPC(grpcListener, service, serverOptions) })
+	g.Go(func() error { return ServeHTTP(httpListener, service) })
+	g.Go(func() error { return m.Serve() })
+
+	log.Fatal(g.Wait())
+}
+
 func main(){
 
 }
